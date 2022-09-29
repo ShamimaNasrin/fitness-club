935f7e86ceb74b8db09a9fcf2caf630c53a2e034
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-09-29 16:19:38 +0600

    grid style

--- src/components/Exsercises/Exsercises.js
+++ src/components/Exsercises/Exsercises.js
@@ -4,19 +4,22 @@ import './Exsercises.css';
 
 const Exsercises = (props) => {
     //console.log(props.workoutName);
-    const {name, details, age, time, img} = props.workoutName;
+    const { name, details, age, time, img } = props.workoutName;
     return (
         <div className='workout'>
-            <img src={img} alt="workout"></img>
-            <div className='workout-info'>
-                <h5 className='workout-name'>{name}</h5>
-                <p className='workout-details'>{details}</p>
-                <p>For age: {age}</p>
-                <p>Time required: {time}s</p>
-                <button  className='btn-add'>
-                <p className='btn-text'>Add activity</p>
-                
-            </button>
+            <div className="card">
+                <img src={img} alt="workout"></img>
+                <div className='workout-info'>
+                    <h5 className='workout-name'>{name}</h5>
+                    <p className='workout-details'>{details}</p>
+                    <p>For age: {age}</p>
+                    <p>Time required: {time}s</p>
+                    <div className='btn-div'>
+                    <button className='btn-add'>
+                        <p className='btn-text'>Add activity</p>
+                    </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
